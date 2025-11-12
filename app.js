f77b75f8b43b5ba8a111512119e069c8741ccf54
Divyanshu-Mishra9620 <dvbeast465@gmail.com>
2025-11-12 11:20:08 +0530

    Enable CORS for frontend URL uber-b-4vyh.vercel.app

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-const dotenv=require("dotenv")
-dotenv.config()
-const cors=require("cors")
-const express=require("express")
-const {connectToDb}=require("./db/db")
-const userRoute=require("./routes/user.routes")
-const cookieParser=require("cookie-parser")
-const captainRoute=require("./routes/captain.routes")
-const mapsRoute=require("./routes/maps.routes")
-const rideRoute=require("./routes/ride.routes")
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
+const express = require("express");
+const { connectToDb } = require("./db/db");
+const userRoute = require("./routes/user.routes");
+const cookieParser = require("cookie-parser");
+const captainRoute = require("./routes/captain.routes");
+const mapsRoute = require("./routes/maps.routes");
+const rideRoute = require("./routes/ride.routes");
 
-const app=express();
+const app = express();
 connectToDb();
 
-app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser())
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:3000",
+    "https://uber-b-4vyh.vercel.app",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-app.use("/users",userRoute);
-app.use("/captains",captainRoute)
-app.use("/maps",mapsRoute);
-app.use("/rides",rideRoute)
+app.use("/users", userRoute);
+app.use("/captains", captainRoute);
+app.use("/maps", mapsRoute);
+app.use("/rides", rideRoute);
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.get("/",(req,res)=>{
-    res.send("Hello World")
-})
-
-module.exports=app;
+module.exports = app;
