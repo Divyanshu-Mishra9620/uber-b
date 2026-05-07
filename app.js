dd997b06822bd19375587015b0840f8108913bc3
Divyanshu-Mishra9620 <dvbeast465@gmail.com>
2026-05-07 19:45:59 +0530

    updated

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
       "http://127.0.0.1:5173",
+      "https://uber-b-4vyh.vercel.app",
       "http://127.0.0.1:3000",
       process.env.FRONTEND_URL || "http://localhost:5173",
     ],
